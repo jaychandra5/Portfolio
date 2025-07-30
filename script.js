ffd9fa536f1e24634b64b43d2693352e743bcbb3
jaychandra5 <jaychandra9931@gmail.com>
2025-07-31 00:42:58 +0530

    making responsive

--- script.js
+++ script.js
@@ -36,3 +36,9 @@ form.addEventListener('submit', function(e) {
     statusText.textContent = "Please fill in all fields.";
   }
 });
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
